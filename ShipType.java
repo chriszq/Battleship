4079487590f1546f03d6d4dc0c8ca99546da852a
chriszq <cchhristopher@gmail.com>
2018-05-08 22:08:01 +0100

    added name and length field values

--- ShipType.java
+++ ShipType.java
@@ -1,7 +1,24 @@
 public enum ShipType {
   
-  CARRIER,
-  BATTLESHIP,
-  CRUISER,
-  DESTOYER;
+  CARRIER ("Carrier", 5),
+  BATTLESHIP ("Battleship", 4),
+  CRUISER ("Cruiser", 3),
+	SUBMARINE ("Submarine", 3),
+  DESTOYER ("Destroyer", 2);
+
+	private final String shipName;
+	private final int shipLength;
+
+	private ShipType(String shipName, int shipLength) {
+		this.shipName = shipName;
+		this.shipLength = shipLength;
+	}
+
+	public String getShipName() {
+		return this.shipName;
+	}
+
+	public int getShipLength() {
+		return this.shipLength;
+	}
 }
