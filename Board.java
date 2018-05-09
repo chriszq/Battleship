78c0d22325d3d7e3f1ddf646d88d243751f903f9
chriszq <cchhristopher@gmail.com>
2018-05-09 22:14:12 +0100

    spacing housekeeping

--- Board.java
+++ Board.java
@@ -1,56 +1,56 @@
 public class Board {
 
-	private static final int OCEAN_DIM = 10;
+  private static final int OCEAN_DIM = 10;
 
-	private Water[][] ocean;
-	private Ship[] fleet;
-	private int shotCount;
-	private int hitCount;
-	private int shipsSunkCount;
+  private Water[][] ocean;
+  private Ship[] fleet;
+  private int shotCount;
+  private int hitCount;
+  private int shipsSunkCount;
 
-	public Board() {
-		this.ocean = new Water[OCEAN_DIM][OCEAN_DIM];
-		
-		for (int i = 0; i < OCEAN_DIM; i++) {
-			for (int j = 0; j < OCEAN_DIM; j++) {
-				this.ocean[i][j] = new Water();
-			}
-		}
+  public Board() {
+    this.ocean = new Water[OCEAN_DIM][OCEAN_DIM];
 
-		this.fleet = new Ship[ShipType.values().length];
+    for (int i = 0; i < OCEAN_DIM; i++) {
+      for (int j = 0; j < OCEAN_DIM; j++) {
+        this.ocean[i][j] = new Water();
+      }
+    }
 
-		int count = 0;
-		for (ShipType shipType : ShipType.values()) {
-			this.fleet[count] = new Ship(shipType);
-			count++;
-		}
-	}
+    this.fleet = new Ship[ShipType.values().length];
+
+    int count = 0;
+    for (ShipType shipType : ShipType.values()) {
+      this.fleet[count] = new Ship(shipType);
+      count++;
+    }
+  }
   
-	public Water[][] getOcean() {
-		return this.ocean;
-	}
+  public Water[][] getOcean() {
+    return this.ocean;
+  }
 
-	public Ship[] getFleet() {
-		return this.fleet;
-	}
+  public Ship[] getFleet() {
+    return this.fleet;
+  }
 
-	public void printOcean() {
-		for (int i = 0; i < OCEAN_DIM; i++) {
-			System.out.println();
-			for (int j = 0; j < OCEAN_DIM; j++) {
-				System.out.printf("%s ", ocean[i][j]);
-			}
-		}
-		System.out.println();
-	}
+  public void printOcean() {
+    for (int i = 0; i < OCEAN_DIM; i++) {
+      System.out.println();
+      for (int j = 0; j < OCEAN_DIM; j++) {
+        System.out.printf("%s ", ocean[i][j]);
+      }
+    }
+    System.out.println();
+  }
 
-	public boolean isFleetSunk() {
-		for (Ship x : fleet) {
-			if (!x.isShipSunk()) {
-				return false;
-			}
-		} 
-		return true;
-	}
+  public boolean isFleetSunk() {
+    for (Ship x : fleet) {
+      if (!x.isShipSunk()) {
+        return false;
+      }
+    } 
+    return true;
+  }
 
 }
