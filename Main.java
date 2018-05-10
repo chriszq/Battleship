5e53ad604e075520ad819bc893de99174fef2bd6
chriszq <cchhristopher@gmail.com>
2018-05-10 22:29:42 +0100

    moved all test functions to test class

--- Main.java
+++ Main.java
@@ -1,36 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String[] args) {
 
-  //testing some methods
-
-    Board board3 = new Board();
-    for (Ship x : board3.getFleet()) {
-      System.out.println(x.getShipType());
-    }
-
-    board3.getFleet()[0].putShipAtRandom(board3.getOcean());
-    board3.getFleet()[1].putShipAtRandom(board3.getOcean());
-    board3.getFleet()[2].putShipAtRandom(board3.getOcean());
-    board3.getFleet()[3].putShipAtRandom(board3.getOcean());
-    board3.getFleet()[4].putShipAtRandom(board3.getOcean());
-
-    Scanner kb = new Scanner(System.in);
-    int row;
-    int col;
-
-
-    while(!board3.isFleetSunk()) {
-    System.out.print("target row: ");
-    row = Integer.parseInt(kb.nextLine());
-    System.out.print("target col: ");
-    col = Integer.parseInt(kb.nextLine());
-
-    board3.getOcean()[row][col].fireAt();
-    board3.printOcean();
-    }
+    Test test = new Test();
+    test.runTest();
   }  
 
 }
