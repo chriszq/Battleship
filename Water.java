01c9fe73ce9d2841db39015fec912d2f72c97883
chriszq <cchhristopher@gmail.com>
2018-05-11 21:06:35 +0100

    Water class now implements Grid

--- Water.java
+++ Water.java
@@ -1,31 +1,45 @@
-public class Water {
-  
-  protected boolean hitStatus;
+public class Water implements Grid {
+
+  private HitStatus hitStatus;
 
   public Water() {
-    this.hitStatus = false;
-  }
-
-  public boolean getHitStatus() {
-    return this.hitStatus;
-  }
-
-  public void setHitStatus(boolean status) {
-    this.hitStatus = status;
+    this.hitStatus = HitStatus.UNHIT;
   }
 
   @Override
-  public String toString() {
-    return (hitStatus) ? "x" : "-";
+  public HitStatus getHitStatus() {
+    return this.hitStatus;
   }
 
-  public void fireAt() {
-    if (!getHitStatus()) {
-      setHitStatus(true);
-      System.out.println("you missed");
-    } else {
-      System.out.println("you already fired at this spot");
+  @Override
+  public char getIcon() {
+    switch (hitStatus) {
+      case UNHIT:
+        return '-';
+      case HIT:
+        return 'x';
+      default:
+        return '?';
     }
   }
 
+  @Override
+  public void fireAt() {
+    switch (hitStatus) {
+      case UNHIT:
+        setHitStatus(HitStatus.HIT);
+        System.out.println("you missed");
+        break;
+      case HIT:
+        System.out.println("you already fired at this spot");
+        break;
+      default:
+        System.out.println("error occured. cannot sink empty spot ");
+        break;
+    }
+  }
+
+  public void setHitStatus(HitStatus hitStatus) {
+    this.hitStatus = hitStatus;
+  }
 }
