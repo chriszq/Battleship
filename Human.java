fea7f9c710c7ade3a22adf09efa550f736148e62
chriszq <cchhristopher@gmail.com>
2018-05-15 21:20:58 +0100

    added Human class

---
+++ Human.java
@@ -0,0 +1,14 @@
+public class Human implements Player {
+
+  private Board board;
+
+  @Override
+  public void placeFleetOnBoard() {
+
+  }
+
+  @Override
+  public void fireAt(Board board) {
+
+  }
+}
