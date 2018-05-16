c8d13cde8c59247ad4408f0a76857c7158eaa1e7
chriszq <cchhristopher@gmail.com>
2018-05-16 21:07:35 +0100

    input validation for placeFleetOnBoard

--- Human.java
+++ Human.java
@@ -13,29 +13,45 @@ public class Human implements Player {
     Orientation orientation = Orientation.HORIZONTAL;
     int row = 0;
     int col = 0;
+    String temp = " ";
 
     Scanner kb = new Scanner(System.in);
 
     for (Ship x : board.getFleet()) {
       System.out.printf("Orientation of %s (h/v): ", x.getShipType().getName());
-        String userInput = kb.nextLine();
-        switch (userInput) {
-          case "h":
-            orientation = Orientation.HORIZONTAL;
-            break;
-          case "v":
-            orientation = Orientation.VERTICAL;
-            break;
-        }
-
+      while (!kb.hasNext("[hv]{1}")) {
+      System.out.printf("Invalid input. Must be \"h\" or \"v\".");
+      System.out.printf("\nOrientation of %s (h/v): ", x.getShipType().getName());
+      kb.next();
+      }
+      temp = kb.next();
+      switch (temp) {
+        case "h":
+          orientation = Orientation.HORIZONTAL;
+          break;
+        case "v":
+          orientation = Orientation.VERTICAL;
+          break;
+      }
       x.setOrientation(orientation);
 
       System.out.printf("%s bow row number: ", x.getShipType().getName());
-      row = Integer.parseInt(kb.nextLine());
-      System.out.printf("%s bow column number: ", x.getShipType().getName());
-      col = Integer.parseInt(kb.nextLine());
+      while (!kb.hasNext("[0-9]{1}")) {
+        System.out.printf("Invalid row number. Must be integer in the range [0,9].");
+        System.out.printf("\n%s bow row number: ", x.getShipType().getName());
+        kb.next();
+      }
+      row = kb.nextInt();
 
-      if (!board.isShipOverlap(row, col, x) && !board.isShipOutOfBounds(x, row, col)) {
+      System.out.printf("%s bow column number: ", x.getShipType().getName());
+      while (!kb.hasNext("[0-9]{1}")) {
+        System.out.printf("Invalid column number. Must be integer in the range [0,9].");
+        System.out.printf("\n%s bow column number: ", x.getShipType().getName());
+        kb.next();
+      }
+      col = kb.nextInt();
+
+      if (!board.isShipOverlap(row, col, x) && !board.isShipOutOfBounds(row, col, x)) {
         board.putShipAt(row, col, x);
       }
 
